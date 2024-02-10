32e1b3d357b958cd12749ed4e187498ab604c895
larbimahmoud <mahmoud@omegup.tn>
2024-02-10 12:09:30 +0100

    change images

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ function App() {
 
   const classes = useStyle()
  const images = [
-  'https://via.placeholder.com/800x400.png?text=Image+5',
-  'https://via.placeholder.com/800x400.png?text=Image+5',
+  'https://res.cloudinary.com/dehvd9oqm/image/upload/v1707560702/Totale%20Renov/WhatsApp_Image_2024-02-10_at_11.06.07_AM_uet3qa.jpg',
+  'https://res.cloudinary.com/dehvd9oqm/image/upload/v1707560876/Totale%20Renov/WhatsApp_Image_2024-02-10_at_11.08.35_AM_knzuu2.jpg',
   'https://res.cloudinary.com/dehvd9oqm/image/upload/v1707561002/Totale%20Renov/WhatsApp_Image_2024-02-10_at_11.10.54_AM_ri12rw.jpg',
-  'https://via.placeholder.com/800x400.png?text=Image+5',
+  'https://res.cloudinary.com/dehvd9oqm/image/upload/v1707560835/Totale%20Renov/WhatsApp_Image_2024-02-10_at_11.07.19_AM_1_uj8jti.jpg',
   // Add more image URLs as needed
 ]
 
